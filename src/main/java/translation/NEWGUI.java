cbc265305d266b744b59e4da09c3bd3af0c9198a
s7electric <samiacobazzi@gmail.com>
2025-09-22 16:56:13 -0400

    frankenstein newgui with no output

---
+++ src/main/java/translation/NEWGUI.java
@@ -0,0 +1,115 @@
+package translation;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+
+
+// TODO Task D: Update the GUI for the program to align with UI shown in the README example.
+//            Currently, the program only uses the CanadaTranslator and the user has
+//            to manually enter the language code they want to use for the translation.
+//            See the examples package for some code snippets that may be useful when updating
+//            the GUI.
+public class NEWGUI {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            JPanel languagePanel = new JPanel();
+            languagePanel.add(new JLabel("Language:"));
+
+            Translator translator = new JSONTranslator();
+
+            // create combobox, add country codes into it, and add it to our panel
+            JComboBox<String> languageComboBox = new JComboBox<>();
+            for(String countryCode : translator.getLanguageCodes()) {
+                languageComboBox.addItem(countryCode);
+            }
+            languagePanel.add(languageComboBox);
+
+            // add listener for when an item is selected.
+            languageComboBox.addItemListener(new ItemListener() {
+
+                /**
+                 * Invoked when an item has been selected or deselected by the user.
+                 * The code written for this method performs the operations
+                 * that need to occur when an item is selected (or deselected).
+                 *
+                 * @param e the event to be processed
+                 */
+                @Override
+                public void itemStateChanged(ItemEvent e) {
+
+                    if (e.getStateChange() == ItemEvent.SELECTED) {
+                        String country = languageComboBox.getSelectedItem().toString();
+                        JOptionPane.showMessageDialog(null, "user selected " + country + "!");
+                    }
+                }
+
+
+            });
+
+            JPanel countryPanel = new JPanel();
+            languagePanel.setLayout(new GridLayout(0, 2));
+            languagePanel.add(new JLabel("Language:"), 0);
+
+
+
+            String[] items = new String[translator.getCountryCodes().size()];
+
+            JComboBox<String> countryComboBox = new JComboBox<>();
+            int i = 0;
+            for(String countryCodes : translator.getCountryCodes()) {
+                items[i++] = countryCodes;
+            }
+
+            // create the JList with the array of strings and set it to allow multiple
+            // items to be selected at once.
+            JList<String> list = new JList<>(items);
+            list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+            // place the JList in a scroll pane so that it is scrollable in the UI
+            JScrollPane scrollPane = new JScrollPane(list);
+            languagePanel.add(scrollPane, 1);
+
+
+            list.addListSelectionListener(new ListSelectionListener() {
+
+                /**
+                 * Called whenever the value of the selection changes.
+                 *
+                 * @param e the event that characterizes the change.
+                 */
+                @Override
+                public void valueChanged(ListSelectionEvent e) {
+
+                    int[] indices = list.getSelectedIndices();
+                    String[] items = new String[indices.length];
+                    for (int i = 0; i < indices.length; i++) {
+                        items[i] = list.getModel().getElementAt(indices[i]);
+                    }
+
+                    JOptionPane.showMessageDialog(null, "User selected:" +
+                            System.lineSeparator() + Arrays.toString(items));
+
+                }
+            });
+
+            JPanel mainPanel = new JPanel();
+            mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+            mainPanel.add(countryPanel);
+            mainPanel.add(languagePanel);
+
+            JFrame frame = new JFrame("Country Name Translator");
+            frame.setContentPane(mainPanel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+
+
+        });
+    }
+}
